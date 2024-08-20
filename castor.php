e144a22baac2b267aad92699ebff642f51eee2a6
Benoit Foujols <benoit@foujols.com>
2024-08-20 20:09:24 +0200

    #2 update Castor Runner

--- castor.php
+++ castor.php
@@ -4,11 +4,18 @@ use Castor\Attribute\AsTask;
 
 use function Castor\io;
 use function Castor\capture;
+use function Castor\run;
 
-#[AsTask(description: 'Welcome to Castor!')]
-function hello(): void
+#[AsTask(name: 'doc:api', description: 'Génération de la documentation de l\'API')]
+function generate_documentation_api(): void
 {
-    $currentUser = capture('whoami');
+    run('php vendor/bin/openapi app -o openapi.yaml');
+    run('docker run --rm -v $PWD:/spec redocly/cli build-docs openapi.yaml --output=./app/Template/api/redoc.html');
+}
 
-    io()->title(sprintf('Hello %s!', $currentUser));
+#[AsTask(name: 'install', description: 'Génération de la documentation de l\'API')]
+function install(): void
+{
+    run('docker pull redocly/cli');
+    run('composer install');
 }
