f784c83f2d563bca0610947aa0a23088ec5d6b67
Benoit Foujols <benoit@foujols.com>
2024-09-11 13:51:30 +0200

    #9 add data in docker image

--- app/Controller/api/openapi.php
+++ app/Controller/api/openapi.php
@@ -4,7 +4,7 @@ namespace Controller\api;
 
 use OpenApi\Attributes;
 
-#[Attributes\Info(title: 'API VELIKO', version: '0.4', description: 'API VELIKO est une API de gestion des stations de vélos en libre service')]
+#[Attributes\Info(title: 'API VELIKO', version: '0.5', description: 'API VELIKO est une API de gestion des stations de vélos en libre service')]
 #[Attributes\Server(url: 'http://localhost:8042')]
 #[Attributes\Contact(email: 'Benoit.Foujols@ac-creteil.fr', name: 'Benoit Foujols')]
 #[Attributes\License(name: 'MIT', url: 'https://opensource.org/licenses/MIT')]
